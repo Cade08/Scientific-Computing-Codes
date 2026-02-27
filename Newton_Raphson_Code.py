5cbaf22c26b04b10289406fba4344bdb29fd33ea
Samuel Edwards <165111988+Cade08@users.noreply.github.com>
2026-02-27 13:05:30 -0600

    Newton_Raphson_Code.py

--- Newton_Raphson_Code.py
+++ Newton_Raphson_Code.py
@@ -1,6 +1,7 @@
 #Newton-Raphson Method
 
 import sympy as sp
+import matplotlib.pyplot as plt
 
 x = sp.symbols('x')
 
@@ -8,14 +9,26 @@ x_val = float(input("Enter the variable x you would like to start with: "))
 
 n = int(input("Enter the number of iterations you would like to run: "))
 
-f = x**2 - x + 1
-df = sp.diff(f, x)
+f = x**2 - 2 
+df = sp.diff(f, x)   
+
+errors = []
 
 for i in range(n):
     x_old = x_val
-    a = f.subs(x, x_val)      
-    b = df.subs(x, x_val)     
+    a = f.subs(x, x_val)     
+    b = df.subs(x, x_val) 
     x_val = x_val - a/b
-    print("The current x value is:", x_old)
-    print("The value of the slope is:", b)
-    print("The new x is:", x_val, "and it jumped by", x_val - x_old)
+    print("\nIteration", i)
+    print("x_n =", x_old)
+    print("Slope =", b)
+    print("New x =", x_val, " Jump =", x_val - x_old)
+    
+    errors.append(abs(float(f.subs(x, x_val))))
+    
+plt.semilogy(errors, marker='o')
+plt.xlabel("Iteration")
+plt.ylabel("Absolute value of f(x_n)")
+plt.title("Newton Convergence")
+plt.grid(True)
+plt.show()
